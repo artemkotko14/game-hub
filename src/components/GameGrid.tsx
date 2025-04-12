b620037839c3e0bac371551610f548a0b25eca63
Artem Kotko <artem.kotko@ukr.net>
2025-04-12 18:50:03 +0700

    Refactor skeleton in GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,20 +11,28 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   if (error) return <Text>{error}</Text>;
+  if (isLoading)
+    return (
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+        padding="10px"
+        spacing={5}
+      >
+        {skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    );
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
       padding="10px"
       spacing={5}
     >
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
-          </GameCardContainer>
-        ))}
       {data.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
